64d9080c2c1d4794ce755ba887a4a3669bcbfa27
defa1lt <kdustinov@yandex.ru>
2026-05-04 14:57:42 +0300

    fix 3

--- config/config.go
+++ config/config.go
@@ -10,18 +10,22 @@ import (
 func NewConfig() (*ConfigModel, error) {
 	var cfg ConfigModel
 	v := viper.New()
-	v.AddConfigPath("/etc/github.com/poshagator/content-service")
+	v.AddConfigPath("/etc/content-service")
+	v.AddConfigPath("./config")
+	v.AddConfigPath(".")
 	v.SetConfigName("config")
 	v.SetConfigType("yaml")
 
+	v.AutomaticEnv()
+
 	err := v.ReadInConfig()
 	if err != nil {
-		slog.Error("fail to read config", err)
+		slog.Error("fail to read config", "error", err)
 		return &cfg, err
 	}
 	err = v.Unmarshal(&cfg)
 	if err != nil {
-		slog.Error("", fmt.Errorf("unable to decode config into struct, %w", err))
+		slog.Error("unable to decode config into struct", "error", err)
 		return &cfg, err
 	}
 	return &cfg, nil
